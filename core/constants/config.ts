2ef7db7fda69bec444e95d150741c71216469266
iLiranS <lirangamerz@gmail.com>
2026-05-19 20:29:38 +0300

    chore: bump app version to 0.5.1 and macOS bundle version to 40

--- core/constants/config.ts
+++ core/constants/config.ts
@@ -1,6 +1,6 @@
 // Bump this string every time you push an EAS Update OR a Tauri build.
 // This is the version your Next.js changelog.json will look for.
-export const APP_RELEASE_VERSION = "0.5.0";
+export const APP_RELEASE_VERSION = "0.5.1";
 
 // ~ LOCAL LIMITS ~ 
 // Later we can make these dynamic based backend app-config storage bucket
