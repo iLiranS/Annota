ee741ffb83a30992a376d4c0ba89402d76f8412d
iLiranS <lirangamerz@gmail.com>
2026-05-22 18:39:34 +0300

    chore: bump app bundle version to 43 and release version to 0.5.3

--- core/constants/config.ts
+++ core/constants/config.ts
@@ -1,6 +1,6 @@
 // Bump this string every time you push an EAS Update OR a Tauri build.
 // This is the version your Next.js changelog.json will look for.
-export const APP_RELEASE_VERSION = "0.5.2";
+export const APP_RELEASE_VERSION = "0.5.3";
 
 // ~ LOCAL LIMITS ~ 
 // Later we can make these dynamic based backend app-config storage bucket
